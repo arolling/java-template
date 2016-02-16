941df572222e754708fb3fdde9f3d75627555b08
Abigail Rolling and Cassandra Culkins <arolling@gmail.com>
2016-02-16 08:59:40 -0800

    improve template

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -8,23 +8,24 @@ import org.junit.ClassRule;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import static org.fluentlenium.core.filter.FilterConstructor.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class AppTest extends FluentTest {
   public WebDriver webDriver = new HtmlUnitDriver();
+  @Override
   public WebDriver getDefaultDriver() {
       return webDriver;
   }
 
   @ClassRule
   public static ServerRule server = new ServerRule();
-  // Unit testing
 
   //Integration testing
   // @Test
   // public void rootTest() {
-  //     goTo("http://localhost:4567/");
-  //     assertThat(pageSource()).contains("Leap year detector");
+  //   goTo("http://localhost:4567/");
+  //   assertThat(pageSource()).contains("Leap year detector");
   // }
 }
