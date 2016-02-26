645fe1cfe668ea9f20f737182830b92de6f1da6d
Abigail Rolling <arolling@gmail.com>
2016-02-26 07:59:17 -0800

    Add database file templates

--- src/test/java/ClassTest.java
+++ src/test/java/ClassTest.java
@@ -1,10 +1,13 @@
 import org.junit.*;
 import static org.junit.Assert.*;
+import org.sql2o.*;
 
 public class ClassTest {
 
   // @Rule
   // public ClearRule clearRule = new ClearRule();
+  // @Rule
+  // public DatabaseRule database = new DatabaseRule();
 
   // UNIT TESTING
 
